40cea2b0959d9df1f7621ad8be67daabdc200342
anwelquizz <arinarodina27@gmail.com>
2024-03-29 22:13:33 +0300

    suckmadick

--- src/huittta.java
+++ src/huittta.java
@@ -3,16 +3,23 @@ public class huittta {
         int[] newAnwell = {33, 49, 65, 2, 78, 14, 88};
         int maxEl = newAnwell[0];
         int minEl = newAnwell[0];
+        int indexMaxEl = 0;
+        int indexMinEl = 0;
         for (int index = 0; index < newAnwell.length; index++) {
             if (maxEl < newAnwell[index]) {
                 maxEl = newAnwell[index];
+                indexMaxEl= index;
             }
 
             if (minEl > newAnwell[index]) {
                 minEl = newAnwell[index];
+                indexMinEl = index;
             }
 
         } System.out.println(maxEl);
         System.out.println(minEl);
+        System.out.println(maxEl-minEl);
+        System.out.println(indexMaxEl);
+        System.out.println(indexMinEl);
     }
 }

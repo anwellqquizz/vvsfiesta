3c8bce8c4dd67e54ce13a660d944f6fed2dabb26
anwelquizz <arinarodina27@gmail.com>
2024-03-29 23:02:52 +0300

    suckmadick

--- src/suckmadick.java
+++ src/suckmadick.java
@@ -1,5 +1,15 @@
+import java.util.Random;
 public class suckmadick {
     public static void main(String[] args) {
-
+        int[] carr = new int[50];
+        int dorogayacar = 0;
+        Random generatorCarr = new Random();
+        for (int index = 0; index < carr.length; index++) {
+            carr[index] = generatorCarr.nextInt(1, 50);
+            if (dorogayacar < carr[index]) {
+                dorogayacar = carr[index];
+            }
+        }
+        System.out.println(dorogayacar);
     }
 }
